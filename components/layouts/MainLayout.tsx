9319a221c813ba6478ec2344bb0762b85d957d35
Percy Avalos <38476784+percyavaur@users.noreply.github.com>
2022-05-28 22:53:40 -0500

    Pre deployment

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode } from "react";
+import Head from "next/head";
+
+import { Navbar } from "../Navbar";
+import styles from "./MainLayout.module.css";
+
+interface Props {
+  children: ReactNode;
+}
+
+export const MainLayout: FC<Props> = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Percy</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
